a401d33ab814e7af37c1272dca704f32652195df
Zdravko Mavkov <mavkov@hotmail.com>
2019-05-15 08:52:04 +0200

    added password encription

--- src/main/java/com/z/ui/WebSecurityConfig.java
+++ src/main/java/com/z/ui/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,12 +31,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
         auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+                .withUser("user").password(encoder.encode("password")).roles("USER");
+//                .and()
+//                .withUser("admin").password(encoder.encode("password")).roles("USER", "ADMIN");
+
+        //second way
+
+//        System.out.println(encoder.encode("password"));
+
+        UserDetails userDetails = User.withUsername("admin")
+                .password("{bcrypt}$2a$10$QJEUBPjP3lwuvCdE5TGtAOSuXySCEykMxsC.Aquszx1zvdXiF3ubm")
+                .roles("USER", "ADMIN")
+                .build();
+
+        auth.inMemoryAuthentication()
+                .withUser(userDetails);
 
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(users.username("user").password("password").roles("USER").build());
+//        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+//        return manager;
+//
+//    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
